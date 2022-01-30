fd12514ba942f9b1f1ecc7d6a069f076f0405a9c
Wibo Kuipers <w.kuipers@filmage.nl>
2022-01-30 20:21:07 +0100

    Setup basic sjon parser

--- src/include/generate.cpp
+++ src/include/generate.cpp
@@ -1,19 +1,30 @@
 #include "generate.h"
-
-// using namespace dg;
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/value.h>
+#include <fstream>
 
 std::string sub_directory = "../tests/";
 
 void dg::generate(std::string directory_name) {
 
-    std::string full_path = sub_directory + directory_name;
+    std::ifstream file("../tests/test.json");
 
-    // Generate directory
-    if (mkdir(full_path.c_str(), 0777) == -1) {
-        std::cerr << "Error: " << strerror(errno) << std::endl;
-    }
+    Json::Value raw_json;
+    Json::Reader json_reader;
+
+    json_reader.parse(file, raw_json);
+
+    std::cout << raw_json["template_name"] << endl;
+
+
+    // std::string full_path = sub_directory + directory_name;
+
+    // // Generate directory
+    // if (mkdir(full_path.c_str(), 0777) == -1) {
+    //     std::cerr << "Error: " << strerror(errno) << std::endl;
+    // }
         
-    else {
-        std::cout << "Sucessfully generated directory structure" << std::endl;
-    }
+    // else {
+    //     std::cout << "Sucessfully generated directory structure" << std::endl;
+    // }
 }
