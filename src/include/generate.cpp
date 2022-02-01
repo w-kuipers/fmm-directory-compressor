e08282cdec06091e38914ae9122aa8f90cf739c3
w-kuipers <97242018+w-kuipers@users.noreply.github.com>
2022-02-01 15:21:28 +0100

    Cleaned up code

--- src/include/generate.cpp
+++ src/include/generate.cpp
@@ -39,13 +39,11 @@ void directory_generate::from_file(string root_directory_name, string file_strin
 
 void directory_generate::create_generation_tree(string root_directory_name, json json_data) {
 
-    
-
-    string root_path = sub_directory + root_directory_name;
-
     // Append root directory to generation tree
+    string root_path = sub_directory + root_directory_name;
     directory_generate::generation_tree[0] = root_path;
 
+    // Call recursive function to walk through JSON tree
     directory_generate::traverse(json_data["structure"], root_path);
 
     return;
@@ -53,12 +51,15 @@ void directory_generate::create_generation_tree(string root_directory_name, json
 
 void directory_generate::traverse(json json_data, string path) {
 
+    // Loop through sublevel
     for (size_t d = 0; d < json_data.size(); d++) {
 
+        // Append current path to generation tree
         string cur_title = json_data[d]["title"];
         string new_path = path + "/" + cur_title;
         directory_generate::generation_tree[directory_generate::generation_tree.size()] = new_path;
 
+        // Check if current level has sublevels, if so call the function again
         if (directory_generate::check_subs(json_data[d]["sub"])) {
             directory_generate::traverse(json_data[d]["sub"], new_path);
         }
