755d92df3e9c9333c8a692cc9d21c0f43163ad57
w-kuipers <97242018+w-kuipers@users.noreply.github.com>
2022-02-08 16:33:22 +0100

    Python wrapper tested on windows

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ import sys
 
 sys.path.insert(0, '../build/Debug')
 
-from generate_directory import generate_file_from_path
+from generate_directory import generate_file_from_path, generate_structure_from_file
 
-generate_file_from_path('Z:/01_PROJECTS\FILMAGE-DIRSTRUCTURE-TEMPLATE', 'Pythontest', './pythonTests/', True)
+# generate_file_from_path('Z:/01_PROJECTS\FILMAGE-DIRSTRUCTURE-TEMPLATE', 'Pythontest', './pythonTests/', True)
 
-# generate('pythonMappie', 'generated_test.json', './pythonTests/')
+generate_structure_from_file('pythonMappie', 'pythonTests/Pythontest.fps', './pythonTests/')
