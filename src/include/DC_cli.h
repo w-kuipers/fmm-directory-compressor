8771cd785621bf6953680a4812ce7e6dce8292a5
w-kuipers <97242018+w-kuipers@users.noreply.github.com>
2022-02-09 18:41:39 +0100

    Added license to all c++ source

--- src/include/DC_cli.h
+++ src/include/DC_cli.h
@@ -1,3 +1,25 @@
+/*
+
+ *  FMM Directory Compressor - Compressing directory structures to a .FPS file.
+
+ *  Copyright (C) 2022  Wibo Kuipers
+ 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  
+ *  If not, see https://github.com/w-kuipers/fmm-directory-compressor/blob/master/LICENSE.
+ 
+ */
+
 #include "generate.h"
 #include "common.h"
 
